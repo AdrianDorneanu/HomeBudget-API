8c98784c090b886e8250014dc169800878c1f0af
Adrian Dorneanu <adrian.dorneanu@cegeka.com>
2023-11-11 10:52:52 +0200

    Added nullity check for CRUD operations

--- BusinessLogicLayer/Interfaces/IBudgetService.cs
+++ BusinessLogicLayer/Interfaces/IBudgetService.cs
@@ -5,8 +5,8 @@ namespace BusinessLogicLayer.Interfaces
     public interface IBudgetService
     {
         Task<IEnumerable<Budget>> GetBudgetsAsync();
-        Task<Budget> GetBudgetByIdAsync(Guid id);
-        Task<Budget> AddBudgetAsync(Budget budget);
+        Task<Budget?> GetBudgetByIdAsync(Guid id);
+        Task<Budget?> AddBudgetAsync(Budget budget);
         Task UpdateBudgetAsync(Budget budget);
         Task DeleteBudgetByIdAsync(Guid id);
     }
